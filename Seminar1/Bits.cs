0767a476d82da424e8336fd7806576d86685f252
XOF0XC0000005 <leva95reva@yahoo.com>
2024-04-15 17:17:21 +0300

    Add files via upload

---
+++ Seminar1/Bits.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seminar1
+{
+    internal class Bits : IBits
+    {
+        public Bits(byte b) 
+        {
+            Value = b;
+            SizeOfValue = sizeof(byte) * 8;
+        }
+
+        public Bits(long b)
+        {
+            Value = b;
+            SizeOfValue = sizeof(long) * 8;
+
+        }
+
+        public Bits(short b)
+        {
+            Value = b;
+            SizeOfValue = sizeof(short) * 8;
+
+        }
+
+        public Bits(int b)
+        {
+            Value = b;
+            SizeOfValue = sizeof(int) * 8;
+
+        }
+
+
+        public long Value { get; private set; } = 0;
+        public int SizeOfValue { get; set; }
+
+        //public bool this[int index]
+        //{
+        //    get
+        //    {
+        //        if (index > 7 || index < 0)
+        //            return false;
+        //        return ((Value >> index) & 1) == 1;
+        //    }
+        //    set
+        //    {
+        //        if (index > 7 || index < 0)
+        //            if (value == true)
+        //                Value = (byte)(Value | (1 << index));
+        //            else
+        //            {
+        //                var mask = (byte)(1 << index);
+        //                mask = (byte)(0xff ^ mask);
+        //                Value &= (byte)(Value & mask);
+        //            }
+        //    }
+        //}
+
+        //public static implicit operator long(Bits b) => b.Value;
+        //public static implicit operator long(long l) => new Bits(l);
+
+        public static implicit operator Bits(byte b) => new Bits(b);
+        public static implicit operator Bits(long b) => new Bits(b);
+        public static implicit operator Bits(int b) => new Bits(b);
+
+
+        public bool GetBit(int i)
+        {
+            if (i > SizeOfValue || i < 0)
+                return false;
+            return ((Value >> i) & 1) == 1;
+        }
+
+        public void SetBit(int i, bool val)
+        {
+            if (i > SizeOfValue || i < 0)
+                if (val)
+                    Value = (byte)(Value | (1 << i));
+                else
+                {
+                    var mask = (byte)(1 << i);
+                    mask = (byte)(0xff ^ mask);
+                    Value &= (byte)(Value & mask);
+                }
+        }
+    }
+}
---
+++ Seminar1/Program.cs
@@ -0,0 +1,79 @@
+﻿using System.Security.Cryptography;
+
+namespace Seminar1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //if (args.Length < 2) 
+            //{
+            //    Console.WriteLine("Недостаточно аргументов");
+            //    return; 
+            //}
+
+            //int.TryParse(args[0], out int correctInt);
+            //int.TryParse(args[1], out int correctInt1);
+
+            //Console.WriteLine(correctInt + correctInt1);
+
+            //if (args.Length == 0) 
+            //{
+            //    Console.WriteLine("Введите хотя бы один аргумент");
+            //    return;
+            //}
+            //else if (args.Length == 1)
+            //{
+            //    double.TryParse(args[0], out double correctDouble);
+            //    Console.WriteLine(correctDouble);
+            //    return;
+            //}
+
+            //double result = 0;
+
+            //foreach (var item in args)
+            //{
+            //    double.TryParse(item, out double correctDouble);
+            //    result += correctDouble;
+            //}
+
+            //Console.WriteLine(result / args.Length);
+
+            //int min = int.MaxValue;
+            //bool isNum = false;
+
+            //if (args.Length == 0)
+            //{
+            //    Console.WriteLine("Вы не ввели аргументы");
+            //    return;
+            //}
+            //else if (args.Length == 1 && int.TryParse(args[0], out int num))
+            //{
+            //    isNum = true;
+            //    Console.WriteLine(num);
+            //    return;
+            //}
+
+            //foreach (var item in args)
+            //{
+            //    if (int.TryParse(item, out int num))
+            //    {
+            //        isNum = true;
+            //        if (min > num) min = num;
+            //    }
+            //}
+
+            //if (!isNum) Console.WriteLine(0);
+            //else Console.WriteLine(min);
+
+            int someInt = 1123123123;
+            Bits bitsInt = someInt;
+
+            byte someByte = 11;
+            Bits bitsByte = someByte;
+
+            long someLong = 1123123123;
+            Bits bitsLong = someLong;
+        }
+    }
+}
